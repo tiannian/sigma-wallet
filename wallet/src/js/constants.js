2981c615100a3c6f6a3e9e2b37edc73cd67148e8
tiannian <dtiannian@gmail.com>
2025-05-02 03:52:20 +0800

    restore constants.js

---
+++ wallet/src/js/constants.js
@@ -0,0 +1,6 @@
+export const PROVIDER_RPC = 'https://assets.sw.openhk.cards/dev/provider.json';
+
+export const NETWORK_TYPE_LIST = [
+  { name: 'EIP155 (Ethereum Compatible)', tag: 'EIP155', value: 'eip155' },
+  { name: 'Solana', tag: 'Solana', value: 'solana' },
+];
